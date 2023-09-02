b3762cc43c7c2359d2966acb499301c81f3849af
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-09-02 22:36:11 +0000

    Add git github-style.css, loader.gif, github-information.js and render them

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,6 +1,23 @@
-function fetchGithubInformation(event){
-    var username =$("#gh-username").val();
-    if(!username) {
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+        <span class="small-name">
+        (@<a href="${user.html_url}" target="blank">${user.login}</a>)
+        </span>
+        </h2>
+        <div class="gh-content">
+        <div class="gh-avatar">
+        <a href="${user.html_url}" target="blank">
+        <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+        </a>
+        </div>
+        <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos:${user.public_repos}</P>
+        </div>`;
+}
+
+function fetchGithubInformation(event) {
+    var username = $("#gh-username").val();
+    if (!username) {
         $("#gh-user-data").html(`<h2>Please enter GitHub Username</h2>`)
         return;
     }
@@ -8,4 +25,23 @@ function fetchGithubInformation(event){
         `<div id="loader">
         <img src="assets/css/loader.gif" alt="loading..."/>
     </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, function (errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+
+            }
+        });
+
 }
